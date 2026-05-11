913a448643b4414d612af628e48b0098b200206b
Iconsteam <iconsteamglobalinfrastructure@gmail.com>
2026-05-11 18:12:25 +0530

    Next Push

--- backend/controllers/breeze.controller.js
+++ backend/controllers/breeze.controller.js
@@ -38,68 +38,106 @@
 
 
 
-import crypto from "crypto";
-import fs from "fs";
-import path from "path";
+// import crypto from "crypto";
+// import fs from "fs";
+// import path from "path";
 
-export const signCart = async (req, res) => {
-  try {
-    const breezeCart = req.body.cart;
+// export const signCart = async (req, res) => {
+//   try {
+//     const breezeCart = req.body.cart;
 
-    console.log("CART:", breezeCart);
+//     console.log("CART:", breezeCart);
 
-    // STRINGIFY CART
+//     // STRINGIFY CART
 
-    const cartString =
-      JSON.stringify(breezeCart);
+//     const cartString =
+//       JSON.stringify(breezeCart);
 
-    // PRIVATE KEY
+//     // PRIVATE KEY
 
-    const privateKey =
-      fs.readFileSync(
-        path.join(
-          process.cwd(),
-          "private-key.pem"
-        ),
-        "utf8"
+//     const privateKey =
+//       fs.readFileSync(
+//         path.join(
+//           process.cwd(),
+//           "private-key.pem"
+//         ),
+//         "utf8"
+//       );
+
+//     // SIGN
+
+//     const signer =
+//       crypto.createSign(
+//         "RSA-SHA256"
+//       );
+
+//     signer.update(cartString);
+
+//     signer.end();
+
+//     const signature =
+//       signer.sign(
+//         privateKey,
+//         "base64"
+//       );
+
+//     return res.json({
+//       success: true,
+
+//       cart: cartString,
+
+//       signature,
+//     });
+
+//   } catch (err) {
+
+//     console.error(
+//       "SIGN ERROR:",
+//       err
+//     );
+
+//     return res.status(500).json({
+//       success: false,
+
+//       error: err.message,
+//     });
+//   }
+// };
+
+
+
+import axios from "axios";
+
+export const signCart =
+  async (req, res) => {
+
+    try {
+
+      const response =
+        await axios.post(
+          "https://apothiki.vercel.app/cart-sign-api",
+          {
+            merchantId:
+              "gemrishi",
+
+            cart:
+              req.body.cart,
+          }
+        );
+
+      return res.json(
+        response.data
       );
 
-    // SIGN
+    } catch (err) {
 
-    const signer =
-      crypto.createSign(
-        "RSA-SHA256"
-      );
+      console.error(err);
 
-    signer.update(cartString);
+      return res.status(500).json({
+        success: false,
+        error: err.message,
+      });
+    }
+  };
 
-    signer.end();
-
-    const signature =
-      signer.sign(
-        privateKey,
-        "base64"
-      );
-
-    return res.json({
-      success: true,
-
-      cart: cartString,
-
-      signature,
-    });
-
-  } catch (err) {
-
-    console.error(
-      "SIGN ERROR:",
-      err
-    );
-
-    return res.status(500).json({
-      success: false,
-
-      error: err.message,
-    });
-  }
-};
+  
