45c9d809ba87df729c98ca6e98612c3bd0f51ecc
Iconsteam <iconsteamglobalinfrastructure@gmail.com>
2026-05-13 13:32:13 +0530

    Next Push

--- frontend/src/utils/blazeCheckout.js
+++ frontend/src/utils/blazeCheckout.js
@@ -697,7 +697,7 @@ export const initBlaze = () => {
           "gemrishi",
 
         environment:
-          "smbrelease",
+          "smbRelease",
 
         // integrationType:
         //   "redirection",
