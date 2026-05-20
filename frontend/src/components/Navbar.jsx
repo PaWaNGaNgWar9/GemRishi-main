4474567adcd54dfe756bfe56f9b3b06bdba98218
Iconsteam <iconsteamglobalinfrastructure@gmail.com>
2026-05-20 17:03:11 +0530

    Navbar UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -520,7 +520,7 @@ export default function Navbar({ handleLoginClick }) {
                 </div>
                 <AnimatePresence>
                   {hoveredMenu === "jewellery" && (
-                    <PremiumDropdown>
+                    <PremiumDropdown wide align="left">
                       <JewelleryModal 
                         onHover={() => setHoveredMenu("jewellery")} 
                         onMouseLeave={() => setHoveredMenu(null)} 
