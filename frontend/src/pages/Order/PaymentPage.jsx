04b4f79ea23b28a8280a67389ed4e2fb7cd47ba5
Iconsteam <iconsteamglobalinfrastructure@gmail.com>
2026-06-02 13:38:35 +0530

    Key ID

--- frontend/src/pages/Order/PaymentPage.jsx
+++ frontend/src/pages/Order/PaymentPage.jsx
@@ -1586,7 +1586,7 @@ function PaymentPage() {
 
             signature: response.data.signature,
 
-            keyId: "xUIoFE0JyzY8NpYx0aIt1",
+            keyId: "BHqOsoFaflqL65A4M0lcT",
 
             customer: {
               countryCode: "91",
@@ -1858,8 +1858,8 @@ function PaymentPage() {
               {/* Button */}
               <div className="w-full h-auto flex items-end mt-6">
                 <button
-                  onClick={handleProceed}
-                  // onClick={handleBreezeProceed}
+                  // onClick={handleProceed}
+                  onClick={handleBreezeProceed}
                   className="w-full max-w-[458px] h-[60px] text-[20px] font-serif text-white bg-[#264A3F] rounded-[10px] cursor-pointer"
                 >
                   {paymentMethod === "cod"
