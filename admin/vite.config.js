70234151aa4fad7091b1a91e9a93970119389668
Iconsteam <iconsteamglobalinfrastructure@gmail.com>
2026-06-11 22:04:11 +0530

    Next Push

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,14 +1,36 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => {
+
+//   const env = loadEnv(mode, process.cwd(), "");
+// 	const BASE_URL = env.VITE_BASE_SITE_URL;
+//   return {
+// 		base: BASE_URL,
+// 		plugins: [react(), tailwindcss()],
+// 	};
+// })
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig(({ mode }) => {
-
   const env = loadEnv(mode, process.cwd(), "");
-	const BASE_URL = env.VITE_BASE_SITE_URL;
+  const BASE_URL = env.VITE_BASE_SITE_URL;
+
   return {
-		base: BASE_URL,
-		plugins: [react(), tailwindcss()],
-	};
-})
+    base: BASE_URL,
+
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+
+    build: {
+      emptyOutDir: false,
+    },
+  };
+});
